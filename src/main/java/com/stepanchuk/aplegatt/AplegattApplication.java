84afa63517780697c2807da1b4bce7691ee3e0fe
Andrii Stepanchuk <andrii.stepanchuk11@gmail.com>
2023-07-29 16:54:14 +0300

    project initialization

---
+++ src/main/java/com/stepanchuk/aplegatt/AplegattApplication.java
@@ -0,0 +1,13 @@
+package com.stepanchuk.aplegatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplegattApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplegattApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stepanchuk/aplegatt/AplegattApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepanchuk.aplegatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplegattApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
